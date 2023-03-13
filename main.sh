e1979571bcc2b8d0a0c34cf211ea768d3502aeea
David Holdeman <dave@holdemanenterprises.com>
2023-03-13 17:47:33 -0500

    ibom in current dir

--- main.sh
+++ main.sh
@@ -3,8 +3,11 @@
 git clone https://github.com/openscopeproject/InteractiveHtmlBom.git
 
 export INTERACTIVE_HTML_BOM_NO_DISPLAY="true"
+
+mkdir ibom
+
 ibom() {
-  python3 InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format "%f" --dest-dir ../ibom --include-nets $@
+  python3 InteractiveHtmlBom/InteractiveHtmlBom/generate_interactive_bom.py --no-browser --name-format "%f" --dest-dir ibom --include-nets $@
 }
 
 PCBS=$(find . -path "$FILES")
